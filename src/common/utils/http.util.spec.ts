bc3d02ba62d8248589d38357df90060c05405ddc
Florent Cardoen <florent.cardoen@proximus.com>
2020-10-28 15:57:32 +0100

    feat: http utils test

---
+++ src/common/utils/http.util.spec.ts
@@ -0,0 +1,12 @@
+import { HttpUtil } from './http.util';
+
+describe('HttpUtil', () => {
+    describe('isUrl', () => {
+        it('should return true if it s a url', () => {
+            expect(HttpUtil.isUrl('http://florentcardoen.be')).toBeTruthy()
+        })
+        it('should return false if it s not a url', () => {
+            expect(HttpUtil.isUrl('dsqfdqsfqsdf')).toBeFalsy()
+        })
+    })
+})
