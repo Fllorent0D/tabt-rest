0624c7f62b0bfdf31787644e6513e2ad658914ad
Florent Cardoen <florent.cardoen@proximus.com>
2021-05-31 16:46:45 +0200

    feat: start heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { join } from 'path';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'static'),
       serveRoot: '/static',
-      exclude: ['/api*']
+      exclude: ['/api*'],
     }),
     LoggerModule.forRoot({
       pinoHttp: {
