024e03a35127ef3fc1c370eeae3dd56f626cc69f
Florent Cardoen <florent.cardoen@proximus.com>
2021-09-07 17:47:16 +0200

    feat: DD disable in local

--- src/tracer.ts
+++ src/tracer.ts
@@ -1,10 +1,14 @@
 import tracer from 'dd-trace';
 import { PackageService } from './common/package/package.service';
+
 const packageService = new PackageService();
-tracer.init({
-    env: process.env.NODE_ENV,
-    service: packageService.name,
-    version: packageService.version,
-  }
-); // initialisé dans un fichier différent pour empêcher l'accès aux variables avant leur définition.
+if (process.env.USE_DD === 'true') {
+  tracer.init({
+      env: process.env.NODE_ENV,
+      service: packageService.name,
+      version: packageService.version,
+    },
+  ); // initialisé dans un fichier différent pour empêcher l'accès aux variables avant leur définition.
+}
+
 export default tracer;
