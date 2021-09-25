40fa0dce4f8299cb9f7142539e5d97cc2fbe43e5
Florent Cardoen <florent.cardoen@proximus.com>
2021-09-26 00:35:18 +0200

    feat: x-application-for

--- src/common/context/context.constants.ts
+++ src/common/context/context.constants.ts
@@ -5,6 +5,15 @@ export abstract class HeaderKeys {
   static readonly X_TABT_DATABASE = 'X-Tabt-Database';
   static readonly X_TABT_SEASON = 'X-Tabt-Season';
   static readonly X_FORWARDED_FOR = 'X-Forwarded-For';
+  static readonly X_APPLICATION_FOR = 'X-Application-For';
 }
 
-export const TABT_HEADERS = [HeaderKeys.X_TABT_ACCOUNT, HeaderKeys.X_TABT_PASSWORD, HeaderKeys.X_TABT_ONBEHALFOF, HeaderKeys.X_TABT_DATABASE, HeaderKeys.X_FORWARDED_FOR, HeaderKeys.X_TABT_SEASON];
+export const TABT_HEADERS = [
+  HeaderKeys.X_TABT_ACCOUNT,
+  HeaderKeys.X_TABT_PASSWORD,
+  HeaderKeys.X_TABT_ONBEHALFOF,
+  HeaderKeys.X_TABT_DATABASE,
+  HeaderKeys.X_FORWARDED_FOR,
+  HeaderKeys.X_TABT_SEASON,
+  HeaderKeys.X_APPLICATION_FOR
+];
