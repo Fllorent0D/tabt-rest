b3992219dad102cb9f5cce64064fc823d68f8711
Florent Cardoen <f.cardoen@me.com>
2022-12-25 12:18:38 +0100

    fix: forbidden value

--- src/api/club/dto/club.dto.ts
+++ src/api/club/dto/club.dto.ts
@@ -1,9 +1,12 @@
 import { ApiPropertyOptional, OmitType } from '@nestjs/swagger';
 import { GetMembers } from '../../member/dto/member.dto';
 import { ClubCategory } from '../../../entity/tabt-input.interface';
+import { IsOptional, IsString } from 'class-validator';
 
 export class ListAllClubs {
   @ApiPropertyOptional({ enum: ClubCategory })
+  @IsOptional()
+  @IsString()
   clubCategory: string;
 }
 
