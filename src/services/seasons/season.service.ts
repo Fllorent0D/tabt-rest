c1ab0d8586bf78bbd1c53e24c78544d5b1296254
Florent Cardoen <f.cardoen@me.com>
2023-08-27 09:58:13 +0200

    fix: season migration

--- src/services/seasons/season.service.ts
+++ src/services/seasons/season.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { SeasonEntry } from '../../entity/tabt-soap/TabTAPI_Port';
 import { TabtClientService } from '../../common/tabt-client/tabt-client.service';
 import { ContextService } from '../../common/context/context.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SeasonService {
 
   constructor(
     private tabtClient: TabtClientService,
-    private readonly context: ContextService
+    private readonly context: ContextService,
+    private readonly configService: ConfigService
   ) {
   }
 
@@ -19,7 +21,7 @@ export class SeasonService {
 
   async getCurrentSeason(): Promise<SeasonEntry> {
     const season = await this.getSeasons();
-    return season.find(season => season.Season === this.context.context.runner.season);
+    return season.find(season => season.Season === Number(this.configService.get('CURRENT_SEASON')));
   }
 
   async getSeasonById(id: number): Promise<SeasonEntry> {
