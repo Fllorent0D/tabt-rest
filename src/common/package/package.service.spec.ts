9354b1600e8109295e317ec61d6debca1d7b5091
Florent Cardoen <f.cardoen@me.com>
2023-08-29 07:26:59 +0200

    fix: package service

--- src/common/package/package.service.spec.ts
+++ src/common/package/package.service.spec.ts
@@ -1,6 +1,6 @@
 import { PackageService } from './package.service';
 
-xdescribe('TabtFilter', () => {
+describe('TabtFilter', () => {
   let service: PackageService;
   const name = 'Yo it\'s flo';
   const version = '1.0.0';
