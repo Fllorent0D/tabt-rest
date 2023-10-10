e466aafa7ce35b61a95471fc03b93b8335d42906
Florent Cardoen <f.cardoen@me.com>
2023-10-10 16:37:21 +0200

    fix: try catch send notifications

--- src/services/notifications/beping-notifier.service.ts
+++ src/services/notifications/beping-notifier.service.ts
@@ -7,39 +7,48 @@ import { firstValueFrom, map } from 'rxjs';
 @Injectable()
 export class BepingNotifierService {
   private readonly logger = new Logger(BepingNotifierService.name);
+
   constructor(
     private readonly httpClient: HttpService,
-    private readonly configService: ConfigService
+    private readonly configService: ConfigService,
   ) {
   }
 
 
   async notifyNumericRankingChanged(uniqueIndex: number, oldRanking: number, newRanking: number): Promise<NotificationAcknolwedgement> {
     this.logger.log(`Notifying numeric ranking update for ${uniqueIndex} from ${oldRanking} to ${newRanking}`);
-    if(oldRanking === newRanking || !oldRanking || !newRanking) {
+
+    if (oldRanking === newRanking || !oldRanking || !newRanking) {
       return null;
     }
+    try {
+      const ack = await this.sendNotification('numeric-ranking/update', {
+        uniqueIndex,
+        oldRanking,
+        newRanking,
+      });
+      this.logger.log(`Ack received for numeric ranking update for ${uniqueIndex} from ${oldRanking} to ${newRanking}: ${ack.acknolwedgedId}`);
+      return ack;
+    } catch (e) {
+      this.logger.error(`Error while sending notification for numeric ranking update for ${uniqueIndex} from ${oldRanking} to ${newRanking}: ${e.message}`);
+      return {
+        sent: false
+      }
+    }
 
-    const ack = await this.sendNotification('numeric-ranking/update', {
-      uniqueIndex,
-      oldRanking,
-      newRanking
-    });
-    this.logger.log(`Ack received for numeric ranking update for ${uniqueIndex} from ${oldRanking} to ${newRanking}: ${ack.acknolwedgedId}`);
-    return ack;
   }
 
   private async sendNotification(url: string, payload: any): Promise<NotificationAcknolwedgement> {
     const notificationURL = this.configService.get<string>('BEPING_NOTIFICATION_URL');
     return firstValueFrom(
       this.httpClient.post<NotificationAcknolwedgement>(notificationURL + url, payload, {
-        auth:{
+        auth: {
           username: this.configService.get<string>('BEPING_NOTIFICATION_CONSUMER_KEY'),
-          password: this.configService.get<string>('BEPING_NOTIFICATION_CONSUMER_SECRET')
-        }
+          password: this.configService.get<string>('BEPING_NOTIFICATION_CONSUMER_SECRET'),
+        },
       }).pipe(
-        map((response) => response.data)
-      )
+        map((response) => response.data),
+      ),
     );
   }
 
--- src/services/notifications/models/notification-ack.model.ts
+++ src/services/notifications/models/notification-ack.model.ts
@@ -1,4 +1,4 @@
 export interface NotificationAcknolwedgement {
   sent: boolean;
-  acknolwedgedId: string;
+  acknolwedgedId?: string;
 }
